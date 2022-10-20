87c4aeab67a085897fe250afe8e81a2a765d2e00
esteban.juncosa <esteban.juncosa@TLPU687.etab.local>
2022-10-20 14:54:28 +0200

    3.0

--- sujet_3.py
+++ sujet_3.py
@@ -9,4 +9,25 @@ chiffres = [1000, 800, 802, 1000, 1003]
 
 #Exercice 2
 
-#VOIR CLASSES
+class Noeud :
+    
+    def __init__(self,g,v,d) :
+        self.gauche = g
+        self.droite = d
+        self.valeur = v
+        
+        def __str__(self) :
+            return str(self.valeur)
+        
+        def est_une_feuille(self) :
+            return self.gauche is None and self.droit is None
+        
+def expression_infixe(e) :
+    s = ...
+    if e.gauche is not None :
+        s = '(' + s + expression_infixe(...)
+    s += ...
+    if ... is not None :
+        s = s + ... + ...
+    if ... :
+        return s
